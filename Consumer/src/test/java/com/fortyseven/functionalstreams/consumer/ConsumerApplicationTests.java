8aef36cbcefd8c6d2913919b79218fa5cbd73f0f
Jorge Castillo Pérez <jorge.castillo.prz@gmail.com>
2020-04-23 10:02:27 +0200

    Adds consumer project.

---
+++ Consumer/src/test/java/com/fortyseven/functionalstreams/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fortyseven.functionalstreams.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
